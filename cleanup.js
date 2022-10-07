35c317690471d9297475a949e23d1b5b7cbd2de1
Daniel Kales <daniel.kales@gmail.com>
2022-10-07 13:05:33 +0200

    fix: nicer kill

--- cleanup.js
+++ cleanup.js
@@ -3,7 +3,7 @@ const execa = require('execa')
 async function run() {
   console.log('Stopping ssh-agent')
 
-  await execa('kill -9 $(pidof ssh-agent)', { shell: true });
+  await execa('kill -15 $(pidof ssh-agent)', { shell: true });
 }
 
 run()
