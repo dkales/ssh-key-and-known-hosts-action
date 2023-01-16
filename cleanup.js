ecb70bdffbb395cbb2d0749e5fd36fb321d6029b
Daniel Kales <daniel.kales@gmail.com>
2023-01-16 16:30:19 +0100

    feat: improve cleanup

--- cleanup.js
+++ cleanup.js
@@ -1,9 +1,25 @@
 const execa = require('execa')
+const core = require('@actions/core')
 
 async function run() {
   console.log('Stopping ssh-agent')
 
-  await execa('kill -9 $(ps -e | grep -m1 "[s]sh-agent" | awk \'{print $1}\')', { shell: true });
+    try {
+      await execa('kill -9 $(ps -e | grep -m1 "[s]sh-agent" | awk \'{print $1}\')', { shell: true });
+    }
+    catch (error) 
+    {
+      console.log(error)
+    }
+
+    const authSock    = core.getInput('ssh-socket')
+    try {
+      await execa('rm', [authSock], { shell: true });
+    }
+    catch (error) 
+    {
+      console.log(error)
+    }
 }
 
 run()
